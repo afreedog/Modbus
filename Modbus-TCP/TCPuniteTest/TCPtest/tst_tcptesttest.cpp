7848e0f44ccd93b83119f8e6f5ac88c2c647febd
guoaowen <1994370405@qq.com>
2022-01-06 10:44:24 +0800

    调整代码格式

--- Modbus-TCP/TCPuniteTest/TCPtest/tst_tcptesttest.cpp
+++ Modbus-TCP/TCPuniteTest/TCPtest/tst_tcptesttest.cpp
@@ -92,134 +92,6 @@ TCPtestTest::TCPtestTest()
 {
 }
 
-//多线圈字符串合法性判断函数测试数据
-void TCPtestTest::test_coilsLegality_data()
-{
-    QTest::addColumn<QString>("coils");
-    QTest::addColumn<bool>("expected");
-
-    QTest::newRow("0") << "0" << true;
-    QTest::newRow("1") << "1" << true;
-    QTest::newRow("2") << "00000" << true;
-    QTest::newRow("3") << "11111" << true;
-    QTest::newRow("4") << "0011" << true;
-    QTest::newRow("5") << "1100" << true;
-    QTest::newRow("6") << "3" << false;
-    QTest::newRow("7") << "333" << false;
-    QTest::newRow("8") << "02000" << false;
-    QTest::newRow("9") << "12000" << false;
-    QTest::newRow("10") << "0211" << false;
-    QTest::newRow("117") << "1211" << false;
-}
-//多线圈字符串合法性判断函数测试
-void TCPtestTest::test_coilsLegality()
-{
-    QFETCH(QString, coils);
-    QFETCH(bool, expected);
-
-    bool result = widget.coilsLegality(coils);
-    QCOMPARE(result, expected);
-}
-
-//字节反转函数测试数据
-void TCPtestTest::test_byteReverse_data()
-{
-    QTest::addColumn<QString>("targetString");
-    QTest::addColumn<QString>("resultString");
-
-    QTest::newRow("全0") << "00000000" << "00000000";
-    QTest::newRow("01混杂") << "01110110" << "01101110";
-    QTest::newRow("全1") << "11111111" << "11111111";
-}
-//字节反转函数测试
-void TCPtestTest::test_byteReverse()
-{
-    QFETCH(QString, targetString);
-    QFETCH(QString, resultString);
-
-    QString resultStr = targetString;
-    widget.byteReverse(resultStr);
-
-    QCOMPARE(resultStr, resultString);
-}
-
-//连接两个quint8数据为一个quint16数据函数测试数据
-void TCPtestTest::test_BondTwoUint8ToUint16_data()
-{
-    QTest::addColumn<quint8>("preNum");
-    QTest::addColumn<quint8>("afterNum");
-    QTest::addColumn<quint16>("bondNum");
-
-    QTest::newRow("前0后1") << quint8(0) << quint8(255) << quint16(255);
-    QTest::newRow("前1后0") << quint8(255) << quint8(0) << quint16(65280);
-    QTest::newRow("全1") << quint8(255) << quint8(255) << quint16(65535);
-    QTest::newRow("混杂") << quint8(215) << quint8(190) << quint16(55230);
-}
-//连接两个quint8数据为一个quint16数据函数测试
-void TCPtestTest::test_BondTwoUint8ToUint16()
-{
-    QFETCH(quint8, preNum);
-    QFETCH(quint8, afterNum);
-    QFETCH(quint16, bondNum);
-
-    quint16 resultNum = widget.BondTwoUint8ToUint16(preNum, afterNum);
-    QCOMPARE(resultNum, bondNum);
-}
-
-//数组转十六进制字符串函数测试数据
-void TCPtestTest::test_HexByteArrayToHexString_data()
-{
-    QTest::addColumn<QByteArray>("HexByteArr");
-    QTest::addColumn<int>("ConvertLen");
-    QTest::addColumn<int>("pattern");
-    QTest::addColumn<QString>("expected");
-
-    QByteArray arr1;
-    arr1.resize(1);
-    arr1[0] = quint8(0);
-    QTest::newRow("0") << arr1 << 1 << 0 << "00";
-
-    QByteArray arr2;
-    arr2.resize(1);
-    arr2[0] = quint8(255);
-    QTest::newRow("1") << arr2 << 1 << 0 << "ff";
-
-    QByteArray arr3;
-    arr3.resize(2);
-    arr3[0] = quint8(0);
-    arr3[1] = quint8(255);
-    QTest::newRow("2") << arr3 << 2 << 0 << "00ff";
-
-    QByteArray arr4;
-    arr4.resize(2);
-    arr4[0] = quint8(255);
-    arr4[1] = quint8(0);
-    QTest::newRow("3") << arr4 << 2 << 0 << "ff00";
-
-    QByteArray arr5;
-    arr5.resize(2);
-    arr5[0] = quint8(0);
-    arr5[1] = quint8(255);
-    QTest::newRow("4") << arr5 << 2 << 1 << "00 ff ";
-
-    QByteArray arr6;
-    arr6.resize(2);
-    arr6[0] = quint8(255);
-    arr6[1] = quint8(0);
-    QTest::newRow("5") << arr6 << 2 << 1 << "ff 00 ";
-}
-//数组转十六进制字符串函数测试
-void TCPtestTest::test_HexByteArrayToHexString()
-{
-    QFETCH(QByteArray, HexByteArr);
-    QFETCH(int, ConvertLen);
-    QFETCH(int, pattern);
-    QFETCH(QString, expected);
-
-    QString resultStr = widget.HexByteArrayToHexString(HexByteArr, ConvertLen, pattern);
-    QCOMPARE(resultStr, expected);
-}
-
 //读输出线圈0x01和读输出寄存器0x03请求报文构建函数测试数据
 void TCPtestTest::test_RequestMessage0x010x03Build_data()
 {
@@ -850,6 +722,133 @@ void TCPtestTest::test_TCP0X10FuncCodeProcess()
     bool result = widget.TCP0X10FuncCodeProcess(MessageArr, RequestMessageArr);
 
     QCOMPARE(result, expected);
+}
+//多线圈字符串合法性判断函数测试数据
+void TCPtestTest::test_coilsLegality_data()
+{
+    QTest::addColumn<QString>("coils");
+    QTest::addColumn<bool>("expected");
+
+    QTest::newRow("0") << "0" << true;
+    QTest::newRow("1") << "1" << true;
+    QTest::newRow("2") << "00000" << true;
+    QTest::newRow("3") << "11111" << true;
+    QTest::newRow("4") << "0011" << true;
+    QTest::newRow("5") << "1100" << true;
+    QTest::newRow("6") << "3" << false;
+    QTest::newRow("7") << "333" << false;
+    QTest::newRow("8") << "02000" << false;
+    QTest::newRow("9") << "12000" << false;
+    QTest::newRow("10") << "0211" << false;
+    QTest::newRow("117") << "1211" << false;
+}
+//多线圈字符串合法性判断函数测试
+void TCPtestTest::test_coilsLegality()
+{
+    QFETCH(QString, coils);
+    QFETCH(bool, expected);
+
+    bool result = widget.coilsLegality(coils);
+    QCOMPARE(result, expected);
+}
+
+//字节反转函数测试数据
+void TCPtestTest::test_byteReverse_data()
+{
+    QTest::addColumn<QString>("targetString");
+    QTest::addColumn<QString>("resultString");
+
+    QTest::newRow("全0") << "00000000" << "00000000";
+    QTest::newRow("01混杂") << "01110110" << "01101110";
+    QTest::newRow("全1") << "11111111" << "11111111";
+}
+//字节反转函数测试
+void TCPtestTest::test_byteReverse()
+{
+    QFETCH(QString, targetString);
+    QFETCH(QString, resultString);
+
+    QString resultStr = targetString;
+    widget.byteReverse(resultStr);
+
+    QCOMPARE(resultStr, resultString);
+}
+
+//连接两个quint8数据为一个quint16数据函数测试数据
+void TCPtestTest::test_BondTwoUint8ToUint16_data()
+{
+    QTest::addColumn<quint8>("preNum");
+    QTest::addColumn<quint8>("afterNum");
+    QTest::addColumn<quint16>("bondNum");
+
+    QTest::newRow("前0后1") << quint8(0) << quint8(255) << quint16(255);
+    QTest::newRow("前1后0") << quint8(255) << quint8(0) << quint16(65280);
+    QTest::newRow("全1") << quint8(255) << quint8(255) << quint16(65535);
+    QTest::newRow("混杂") << quint8(215) << quint8(190) << quint16(55230);
+}
+//连接两个quint8数据为一个quint16数据函数测试
+void TCPtestTest::test_BondTwoUint8ToUint16()
+{
+    QFETCH(quint8, preNum);
+    QFETCH(quint8, afterNum);
+    QFETCH(quint16, bondNum);
+
+    quint16 resultNum = widget.BondTwoUint8ToUint16(preNum, afterNum);
+    QCOMPARE(resultNum, bondNum);
+}
+
+//数组转十六进制字符串函数测试数据
+void TCPtestTest::test_HexByteArrayToHexString_data()
+{
+    QTest::addColumn<QByteArray>("HexByteArr");
+    QTest::addColumn<int>("ConvertLen");
+    QTest::addColumn<int>("pattern");
+    QTest::addColumn<QString>("expected");
+
+    QByteArray arr1;
+    arr1.resize(1);
+    arr1[0] = quint8(0);
+    QTest::newRow("0") << arr1 << 1 << 0 << "00";
+
+    QByteArray arr2;
+    arr2.resize(1);
+    arr2[0] = quint8(255);
+    QTest::newRow("1") << arr2 << 1 << 0 << "ff";
+
+    QByteArray arr3;
+    arr3.resize(2);
+    arr3[0] = quint8(0);
+    arr3[1] = quint8(255);
+    QTest::newRow("2") << arr3 << 2 << 0 << "00ff";
+
+    QByteArray arr4;
+    arr4.resize(2);
+    arr4[0] = quint8(255);
+    arr4[1] = quint8(0);
+    QTest::newRow("3") << arr4 << 2 << 0 << "ff00";
+
+    QByteArray arr5;
+    arr5.resize(2);
+    arr5[0] = quint8(0);
+    arr5[1] = quint8(255);
+    QTest::newRow("4") << arr5 << 2 << 1 << "00 ff ";
+
+    QByteArray arr6;
+    arr6.resize(2);
+    arr6[0] = quint8(255);
+    arr6[1] = quint8(0);
+    QTest::newRow("5") << arr6 << 2 << 1 << "ff 00 ";
+}
+//数组转十六进制字符串函数测试
+void TCPtestTest::test_HexByteArrayToHexString()
+{
+    QFETCH(QByteArray, HexByteArr);
+    QFETCH(int, ConvertLen);
+    QFETCH(int, pattern);
+    QFETCH(QString, expected);
+
+    QString resultStr = widget.HexByteArrayToHexString(HexByteArr, ConvertLen, pattern);
+    QCOMPARE(resultStr, expected);
 }
 
 QTEST_MAIN(TCPtestTest)
